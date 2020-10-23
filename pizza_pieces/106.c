08a77559b7ef946970787217555b589beb53fb12
mugenmirai <@Tokoloshe6F669>
2020-10-22 20:41:08 -0400

    c-cret

---
+++ pizza_pieces/106.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    double latitude;
+    double longitude;
+    char info[80];
+    int started = 0;
+
+    puts("data=[");
+    while (scanf("%lf,%lf,%79[^\n]", &latitude, &longitude, info) == 3) {
+      if (started)
+        printf(",\n");
+      else
+      started = 1;
+      printf("latitude: %f, longitude: %f, info: '%s')", latitude, longitude, info);
+    }
+    puts("\n]");
+    return 0;
+}
