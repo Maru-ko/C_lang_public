fd8354f605166454d587690a42b96c3a43fc0d6d
Maru-ko <marcosavila@protonmail.com>
2020-10-29 21:17:53 -0400

    z

---
+++ a_math/eq.c
@@ -0,0 +1,19 @@
+#include <math.h>
+//iostream
+int main ()
+{
+double x, x1, x2, a, b, c;
+ct << "Input values of a, b, and c." ;
+cin >> a >> b >> c;
+    if ((b * b - 4 * a * c) > 0)
+    ct << "x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)" &&
+    ct << "x2 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)";
+
+    if else ((b * b - 4 * a * c) = 0)
+    ct << "x = ((-b + sqrt(b * b - 4 * a * c)) / (2 * a)"
+
+    if else ((b * b - 4 * a * c) < 0)
+    ct << "x1 = ((-b + sqrt(b * b - 4 * a * c) * sqrt (-1)) / (2 * a) &&
+    ct << "x2 = ((-b + sqrt(b * b - 4 * a * c) * sqrt (-1)) / (2 * a);
+return (0);
+}
